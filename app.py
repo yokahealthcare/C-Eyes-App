98edb4fb631e6172f4d736d79c57f3a43962abba
Erwin Yonata <86058013+yokahealthcare@users.noreply.github.com>
2023-07-29 20:50:23 +0700

    added STUN

--- app.py
+++ app.py
@@ -2,4 +2,9 @@ import streamlit as st
 from streamlit_webrtc import webrtc_streamer
 # import cv2
 
-webrtc_streamer(key="sample")
+webrtc_streamer(
+    key="sample",
+    rtc_configuration={  # Add this config
+        "iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]
+    }
+)
